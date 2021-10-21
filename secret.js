5a18275a4e3e5720f618848738e4039b7926b3ab
UtsavRaj21 <utsav11115@gmail.com>
2021-10-21 13:09:04 +0530

    update

---
+++ secret.js
@@ -0,0 +1,10 @@
+var firebaseCon = {
+    apiKey: "AIzaSyC6u3iFA_mt69jHvdLyC48O2J4kPtIu7No",
+    authDomain: "resume-builder-95ae9.firebaseapp.com",
+    projectId: "resume-builder-95ae9",
+    storageBucket: "resume-builder-95ae9.appspot.com",
+    messagingSenderId: "412805072255",
+    appId: "1:412805072255:web:9bbc6996e05078bc0e464a"
+  };
+
+  export default firebaseCon;
---
+++ src/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom'; 
+import { Provider } from 'react-redux';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import { createStore, applyMiddleware } from 'redux'
+import rootReducer from './redux/reducers/rootReducer'
+import thunk from 'redux-thunk';
+import { reduxFirestore, getFirestore } from 'redux-firestore';
+import { ReactReduxFirebaseProvider, getFirebase } from 'react-redux-firebase';
+import { createFirestoreInstance } from 'redux-firestore';
+import { composeWithDevTools} from 'redux-devtools-extension'
+
+var firebaseConfig = {
+  apiKey: "AIzaSyC6u3iFA_mt69jHvdLyC48O2J4kPtIu7No",
+  authDomain: "resume-builder-95ae9.firebaseapp.com",
+  projectId: "resume-builder-95ae9",
+  storageBucket: "resume-builder-95ae9.appspot.com",
+  messagingSenderId: "412805072255",
+  appId: "1:412805072255:web:9bbc6996e05078bc0e464a"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.firestore()
+
+const reduxStore = createStore(rootReducer,
+  composeWithDevTools(
+    applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})), 
+    reduxFirestore(firebase) // redux bindings for firestore,  
+  )
+);
+
+
+ReactDOM.render(
+  <Provider store={reduxStore}>
+    <BrowserRouter>
+    <ReactReduxFirebaseProvider
+      firebase={firebase}
+      config={firebaseConfig}
+      dispatch={reduxStore.dispatch}
+      createFirestoreInstance={createFirestoreInstance}
+      >
+      <App />
+    </ReactReduxFirebaseProvider>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
